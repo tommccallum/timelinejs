4278eeef8049c1f9bb548468ab044ad198726fee
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2022-04-29 09:39:37 +0100

    fixed issue with copy command and removed dry-run

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,7 +13,7 @@ date=$(date "+%A %d %B %Y %H:%M")
 cd "$script_dir"
 git pull
 [ ! -d "build" ] && mkdir build
-cp -R "website/*" "build/"
+cp -R website/* build/
 sed -i "s/{{LAST_UPDATED}}/$date/g" build/index.html
 
-[ -n "$destination" -a -d "$destination" ] && rsync --dry-run -rvh "build/" "$destination/" --delete
+[ -n "$destination" -a -d "$destination" ] && rsync -rvh "build/" "$destination/" --delete
