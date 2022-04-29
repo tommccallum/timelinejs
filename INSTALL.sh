55df87e184febdc9f3a222e45d45d25a4f7facc3
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2022-04-29 16:44:44 +0100

    fixing INSTALL.sh to work with cron

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Script to update the timeline website automatically
+if [ "x$PATH" == "x" ]; then
+    export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+fi
 
 # die if there is an error
 set -e 
@@ -11,7 +14,7 @@ script_filename="$(basename "${BASH_SOURCE[0]}")"
 date=$(date "+%A %d %B %Y %H:%M")
 
 cd "$script_dir"
-git pull
+/usr/bin/git pull
 [ ! -d "build" ] && mkdir build
 cp -R website/* build/
 sed -i "s/{{LAST_UPDATED}}/$date/g" build/index.html
